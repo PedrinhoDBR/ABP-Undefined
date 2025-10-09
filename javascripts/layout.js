b280a9e68c78575873794fbe8b7ab4d2865c6706
Rainan Reis <abp.undefined@gmail.com>
2025-10-09 20:32:11 -0300

    index.html falso

--- javascripts/layout.js
+++ javascripts/layout.js
@@ -1,7 +1,7 @@
 
 async function loadHeader() {
     const espacoDoMeuHeader = document.querySelector("#header-socket")
-    const header = await fetch("./Layout/header.html")
+    const header = await fetch("../Layout/header.html")
     const headerText = await header.text()
     espacoDoMeuHeader.innerHTML = headerText
 }
@@ -10,7 +10,7 @@ loadHeader()
 
 async function loadFooter() {
     const espacoDoMeuFooter = document.querySelector("#footer-socket")
-    const footer = await fetch("./Layout/footer.html")
+    const footer = await fetch("../Layout/footer.html")
     const footerText = await footer.text()
     espacoDoMeuFooter.innerHTML = footerText
 }
